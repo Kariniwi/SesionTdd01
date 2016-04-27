b94f9f7762d183ff6a324d4b356a9672ec6d648c
Norma Contreras <nocontre@alumnos.ubiobio.cl>
2016-04-27 12:06:27 -0300

    CommitTestprimero

--- src/test/java/cl/ubb/agil/CalculadoraTest.java
+++ src/test/java/cl/ubb/agil/CalculadoraTest.java
@@ -1,5 +1,9 @@
 package cl.ubb.agil;
-
+import org.junit.Test;
 public class CalculadoraTest {
-
+	public void DosMasTresEsCinco(){
+		Calculadora cal =new Calculadora();
+		int resultado= cal.suma(2,3);
+		assertThat(Resultado,is(5));
+	}
 }
