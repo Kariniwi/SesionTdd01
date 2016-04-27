4c19b9309217d1907c863d2f48a2aed468ab8dbd
Norma Contreras <nocontre@alumnos.ubiobio.cl>
2016-04-27 12:20:16 -0300

    CommitTest2

--- src/test/java/cl/ubb/agil/CalculadoraTest.java
+++ src/test/java/cl/ubb/agil/CalculadoraTest.java
@@ -6,9 +6,16 @@ import static org.hamcrest.CoreMatchers.is;
 
 public class CalculadoraTest {
 	@Test
-	public void DosMasTresEsCinco(){
+	public void dosMasTresEsCinco(){
 		Calculadora cal =new Calculadora();
 		int resultado= cal.sumar(2,3);
 		assertThat(resultado,is(5));
 	}
+	@Test 
+	public void menosUnoMasSieteEsSeis(){
+		Calculadora cal =new Calculadora();
+		int resultado= cal.sumar(-1,7);
+		assertThat(resultado,is(6));
+	}
+	
 }
