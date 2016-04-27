b862233135778f59318d3330fffec1eaf8aa953e
Norma Contreras <nocontre@alumnos.ubiobio.cl>
2016-04-27 12:16:05 -0300

    TestPrimeroPasado

--- src/test/java/cl/ubb/agil/CalculadoraTest.java
+++ src/test/java/cl/ubb/agil/CalculadoraTest.java
@@ -1,9 +1,14 @@
 package cl.ubb.agil;
 import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.hamcrest.CoreMatchers.is;
+
 public class CalculadoraTest {
+	@Test
 	public void DosMasTresEsCinco(){
 		Calculadora cal =new Calculadora();
-		int resultado= cal.suma(2,3);
-		assertThat(Resultado,is(5));
+		int resultado= cal.sumar(2,3);
+		assertThat(resultado,is(5));
 	}
 }
